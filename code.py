6f08233c7f79a6225528268d7d043e9b0f33fb47
Mr-76 <bartmoss77@tutanota.com>
2025-02-01 00:39:25 -0300

    code fix

--- code.py
+++ code.py
@@ -2,34 +2,37 @@ print("Starting")
 
 import board
 
-from kmk.modules.layers import Layers
 from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
 
 keyboard = KMKKeyboard()
 
-keyboard.modules.append(Layers())
-keyboard.col_pins = (board.GP10,board.GP11,board.GP12,)
-keyboard.row_pins = (board.GP15,board.GP14,board.GP13,)
+
+
+keyboard.row_pins = (board.GP0,board.GP1,board.GP2,board.GP3,board.GP4,)
+keyboard.col_pins = (board.GP5,board.GP6,board.GP7,board.GP8,board.GP9,board.GP10,board.GP11,board.GP12,board.GP17,board.GP18,board.GP19,board.GP20,board.GP21,)
+
+
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
-LAYER_TAP = KC.TO(1) # any tap longer than 250ms will be interpreted as a hold
-LAYER_TAP2 = KC.TO(0) # any tap longer than 250ms will be interpreted as a hold
 
 
 keyboard.keymap = [
     # Function Layer
-	[
-		KC.F1,	KC.F2,	KC.F3,
-		KC.F4,	KC.F5,	KC.F6,
-		KC.F7,  KC.F8,  LAYER_TAP,	
-	],
     [
-        KC.A,KC.B,KC.C,
-        KC.D,KC.E,KC.F,
-        KC.G,KC.H,LAYER_TAP2,
-    ],
+        KC.ESC,    KC.N1,    KC.N2,    KC.N3,    KC.N4,    KC.N5,    KC.N6,    KC.N7,    KC.N8,   KC.N9,   KC.N0,       KC.MINUS,    KC.BSPC,
+        KC.TAB,    KC.Q,     KC.W,     KC.E,     KC.R,     KC.T,     KC.Y,     KC.U,     KC.O,    KC.P,    KC.LBRACKET, KC.RBRACKET, KC.BSLASH,
+        KC.CLCK,   KC.A,     KC.S,     KC.D,     KC.F,     KC.G,     KC.H,     KC.J,     KC.K,    KC.L,    KC.SCOLON,   KC.QUOTE,    KC.ENTER,
+        KC.LSHIFT, KC.Z,     KC.X,     KC.C,     KC.V,     KC.B,     KC.N,     KC.M,     KC.COMM, KC.DOT,  KC.SLSH,     KC.UP,       KC.RSHIFT,
+        KC.LCTL,   KC.LGUI,  KC.LALT,  KC.SPC,   KC.SPC,   KC.B,     KC.N,     KC.M,     KC.RALT, KC.PSCR, KC.LEFT,     KC.DOWN,     KC.RIGHT,
+    ]
+
 ]
+
+#
+#keyboard.keymap = [
+#    [KC.A,]
+#]
 
 if __name__ == '__main__':
     keyboard.go()
